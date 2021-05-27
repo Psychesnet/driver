6616af5e5bfc5a11004fe814ae797b464c828f4d
eric hsieh <psychesnet@gmail.com>
2021-05-27 15:42:36 +0800

    [cdev] consider 64/32bit linux kernel

--- cdev/src/cdev_ioctl.h
+++ cdev/src/cdev_ioctl.h
@@ -2,11 +2,28 @@
 #define _CDEV_IOCTL_H
 
 #include <linux/ioctl.h>
+#include <asm/byteorder.h>
 
 struct ioctl_cmd {
     unsigned int reg;
     unsigned int offset;
+#ifdef __LP64__
+    unsigned char *buf;      /* 64bit linux，long/pointer both are 8bytes */
+#else
+    /* 32bit linux */
+#ifdef __LITTLE_ENDIAN_BITFIELD
+    unsigned char *buf;      /* 32bit linux, long/pointer bother are 4bytes */
+    unsigned int padding;
+#elif __BIG_ENDIAN_BITFIELD
+    unsigned int padding;
+    unsigned char *buf;
+#else
+#error "Please fix <asm/byteorder.h>"
+#endif
+
+#endif
     unsigned int val;
+    unsigned int val_padding;
 };
 
 #define IOC_MAGIC 'd'
