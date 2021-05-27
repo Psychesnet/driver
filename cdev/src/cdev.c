e9e4a2c2f45de709d70b1e1980c4f73f65cda3c9
eric hsieh <psychesnet@gmail.com>
2021-05-27 08:59:34 +0800

    [cdev] add read/write support and unit test

--- cdev/src/cdev.c
+++ cdev/src/cdev.c
@@ -5,8 +5,9 @@
 #include <linux/fs.h>
 #include <linux/cdev.h>
 #include <linux/sched.h>
+#include <linux/slab.h>
+#include <linux/uaccess.h>
 #include <asm/current.h>
-#include <asm/uaccess.h>
 
 MODULE_LICENSE("Dual BSD/GPL");
 
@@ -15,61 +16,128 @@ MODULE_LICENSE("Dual BSD/GPL");
 // 0 is dynamic allocation
 static int cdevice_major = 0;
 // device count
-static int cdevice_count = 2;
+static int cdevice_count = 1;
 // accept param when insmod
 module_param(cdevice_major, uint, 0);
 static struct cdev cdevice_cdev;
 
+struct cdevice_data {
+    unsigned char val;
+    rwlock_t lock;
+};
+
 static int cdevice_open(struct inode *inode, struct file *file)
 {
+    int ret = 0;
     printk("%s: major:%d minor:%d (pid:%d)\n", __func__,
             imajor(inode), iminor(inode), current->pid);
-    // inode屬於device，所以1個device，只有1份inode存在於kernel
-    // inode主要存major:minor
-    inode->i_private = inode;
-    // file是分個process都有一份，記錄存取device的狀態
-    file->private_data = file;
-    return 0;
+
+    struct cdevice_data *p = kmalloc(sizeof(struct cdevice_data), GFP_KERNEL);
+    if (!p) {
+        printk("%s: no memory\n", __func__);
+        ret = -ENOMEM;
+        goto out;
+    }
+
+    p->val = 0xff;
+    rwlock_init(&p->lock);
+    file->private_data = p;
+out:
+    return ret;
 }
 
 static int cdevice_close(struct inode *inode, struct file *file)
 {
     printk("%s: major:%d minor:%d (pid:%d)\n", __func__,
             imajor(inode), iminor(inode), current->pid);
+    if (file->private_data) {
+        kfree(file->private_data);
+    }
     return 0;
+}
+
+ssize_t cdevice_write(struct file *file, const char __user *buf,
+        size_t count, loff_t *f_pos)
+{
+    struct cdevice_data *p = file->private_data;
+    unsigned char val;
+    ssize_t write_count;
+
+    printk("%s: count %lu pos %lld\n", __func__, count, *f_pos);
+
+    if (count >= 1) {
+        if (copy_from_user(&val, buf, 1)) {
+            write_count = -EFAULT;
+            goto out;
+        }
+        write_lock(&p->lock);
+        p->val = val;
+        write_unlock(&p->lock);
+    }
+    write_count = count;
+out:
+    return write_count;
+}
+
+ssize_t cdevice_read(struct file *file, char __user *buf,
+        size_t count, loff_t *f_pos)
+{
+    struct cdevice_data *p = file->private_data;
+    unsigned char val;
+    ssize_t read_count;
+    int i = 0;
+
+    read_lock(&p->lock);
+    val = p->val;
+    read_unlock(&p->lock);
+
+    printk("%s: count %lu pos %lld\n", __func__, count, *f_pos);
+
+    for (i = 0; i < count; ++i) {
+        if (copy_to_user(&buf[i], &val, 1)) {
+            read_count = -EFAULT;
+            goto out;
+        }
+    }
+    read_count = count;
+out:
+    return read_count;
 }
 
 struct file_operations cdevice_fops = {
     .open = cdevice_open,
     .release = cdevice_close,
+    .write = cdevice_write,
+    .read = cdevice_read,
 };
 
 static int cdevice_init(void)
 {
-    int ret = -1;
-    dev_t dev;
-    do {
-        dev = MKDEV(cdevice_major, 0);
-        if (alloc_chrdev_region(&dev, 0, cdevice_count, DRIVER_NAME) != 0) {
-            printk("fail to alloc chrdev\n");
-            break;
-        }
-        cdevice_major = MAJOR(dev);
+    int ret = 0;
+    dev_t dev = MKDEV(cdevice_major, 0);
 
-        cdev_init(&cdevice_cdev, &cdevice_fops);
-        cdevice_cdev.owner = THIS_MODULE;
-
-        if (cdev_add(&cdevice_cdev, MKDEV(cdevice_major, 0), cdevice_count) != 0) {
-            printk("fail to add cdevice\n");
-            break;
-        }
-        ret = 0;
-        printk(KERN_ALERT "%s driver(%d:%d) loaded\n", DRIVER_NAME, MAJOR(dev), MINOR(dev));
-    } while (false);
-    if (ret != 0) {
-        cdev_del(&cdevice_cdev);
-        unregister_chrdev_region(dev, cdevice_count);
+    if (alloc_chrdev_region(&dev, 0, cdevice_count, DRIVER_NAME) != 0) {
+        printk("fail to alloc chrdev\n");
+        ret = -EFAULT;
+        goto out;
     }
+
+    cdevice_major = MAJOR(dev);
+    cdev_init(&cdevice_cdev, &cdevice_fops);
+    cdevice_cdev.owner = THIS_MODULE;
+
+    if (cdev_add(&cdevice_cdev, MKDEV(cdevice_major, 0), cdevice_count) != 0) {
+        printk("fail to add cdevice\n");
+        ret = -EFAULT;
+        goto unreg_cdev;
+    }
+    printk(KERN_ALERT "%s driver(%d:%d) loaded\n", DRIVER_NAME, MAJOR(dev), MINOR(dev));
+
+/*del_cdev:*/
+    /*cdev_del(&cdevice_cdev);*/
+unreg_cdev:
+    unregister_chrdev_region(dev, cdevice_count);
+out:
     return ret;
 }
 
---
+++ test/src/test.cpp
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <errno.h>
+
+#define CDEVICE "/dev/cdevice"
+
+int main(int argc, char *argv[])
+{
+    int fd = 0;
+    char buf[8] = {0};
+    char val = 0xff;
+    ssize_t count = 0;
+    do {
+        fd = open(CDEVICE, O_RDWR);
+        if (fd < 0) {
+            fprintf(stderr, "fail to open %s\n", CDEVICE);
+            break;
+        }
+
+        // let's read something
+        count = read(fd, buf, sizeof(buf));
+        if (count > 0) {
+            for (int i = 0; i < count; ++i) {
+                fprintf(stderr, "%02x ", buf[i]);
+            }
+            fprintf(stderr, "\n");
+        }
+
+        // let's write something
+        val = 0xc;
+        if (write(fd, &val, 1) < 0) {
+            fprintf(stderr, "fail to write %s\n", CDEVICE);
+        }
+
+        // let's read something
+        count = read(fd, buf, sizeof(buf));
+        if (count > 0) {
+            for (int i = 0; i < count; ++i) {
+                fprintf(stderr, "%02x ", buf[i]);
+            }
+            fprintf(stderr, "\n");
+        }
+
+        close(fd);
+    } while (false);
+    return 0;
+}
